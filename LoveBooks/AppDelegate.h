5151759f2857d335a6e1ee853365d21013fc8195
bDino <buskulic.dino@gmail.com>
2014-05-13 14:54:17 +0200

    Initial Commit

---
+++ LoveBooks/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  LoveBooks
+//
+//  Created by Dino on 13/05/14.
+//  Copyright (c) 2014 Haw-Hamburg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ LoveBooks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LoveBooks
+//
+//  Created by Dino on 13/05/14.
+//  Copyright (c) 2014 Haw-Hamburg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
